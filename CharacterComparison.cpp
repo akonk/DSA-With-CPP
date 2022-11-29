5df46d873cb80f0fa86ff4a93c76307c5073be7c
akonk <atulkondhare001@gmail.com>
2022-11-29 16:48:15 +0530

    Changed CharacterComparison Program

--- CharacterComparison.cpp
+++ CharacterComparison.cpp
@@ -9,15 +9,15 @@ int main(){
 
     int c = int(a);
 
-    if( 65< c && c < 90 ){
+    if( 65 <= c && c <= 90 ){
 
         cout<<"This is char is in UpperCase.and Ascii Value is "<<c;;
     }
-    else if (97<c && c<122){
+    else if (97<=c && c<=122){
 
         cout<<"You entered alphabet is in Lowercase. and alphabet ascii value is "<<c;
     }
-    else if( 48 < c  && c <57 ){
+    else if( 48 <= c  && c <= 57 ){
 
 cout<<"You Entered Digit and digit Ascii value is "<<c<<endl;
     
