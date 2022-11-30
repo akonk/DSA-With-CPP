7eee10e3c825170f813d549657d6fc2a395aafc2
akonk <atulkondhare001@gmail.com>
2022-11-30 11:20:03 +0530

    Completed Sum of Even Numbers upto 'N' Input.

---
+++ FindSumOfEvenNumbers.cpp
@@ -0,0 +1,45 @@
+// Using While Loop
+// Find Sum of Even Numbers Upto 'N' Number.
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int i =  2;
+
+int n = 0 ;
+
+int sum = 0;
+
+
+cout<<"Enter a Number";
+
+cin>>n;   
+
+while(i<=n){
+
+
+
+
+ sum = sum + i; //sum = sum + i ;
+ 
+  i =  i + 2 ;
+}
+  cout<<sum;
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+
