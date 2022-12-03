f0242132d65200250974db6b9daf61b47038ce85
akonk <atulkondhare001@gmail.com>
2022-12-03 13:40:31 +0530

    New Star Pattern Programs Added.

---
+++ Star Pattern Programs/Pattern5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int row = 1;
+
+    while (row <= 5)
+    {
+
+        int col = row-1;
+        while (col <= 4)
+        {
+            cout << "*";
+            col ++;
+        }
+        cout << endl;
+        row++;
+    }
+}
